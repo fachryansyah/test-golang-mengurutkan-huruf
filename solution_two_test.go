cbeb210c4d97d82c3e7f19b34fbcdf0f67dfed6a
Fahriansyah <fachryansyah123@gmail.com>
2019-11-10 14:12:51 +0700

    update file testing

--- solution_two_test.go
+++ solution_two_test.go
@@ -3,5 +3,8 @@ package main
 import "testing"
 
 func TestSortWord(test *testing.T) {
-
+	result := SortWord("osama")
+	if result != "aaoms" {
+		t.Errorf("SortWord function was error, want: %s got: %s", "aaoms", result)
+	}
 }
